635eed0f2c0e42bfa7a277e020d14b4495285922
tmamedbekov <tony.mamedbekov@gmail.com>
2017-07-13 13:57:36 -0500

    added the files

---
+++ Project/angularpoc/Controllers/AngularController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Sitecore.Mvc.Extensions;
+
+namespace angularpoc.Controllers
+{
+    public static class Angular
+    {
+        public static IHtmlString RawJsEncodedString<T>(this HtmlHelper<T> htmlHelper, HtmlString str)
+        {
+            var test = str.ToString();
+
+            if (test.IsEmptyOrNull())
+            {
+                return new HtmlString(string.Empty);
+            }
+            return new HtmlString(HttpUtility.JavaScriptStringEncode(test));
+        }
+    }
+}
---
+++ Project/angularpoc/obj/Debug/Package/PackageTmp/angular/scripts/myAngular.js
@@ -0,0 +1,20 @@
+﻿angular.module('sc.sample', [
+  'ngSanitize'
+]).directive('sampleComponent', function () {
+    return {
+        scope: {
+            data: '=sampleComponent'
+        },
+        templateUrl: `./angular/templates/angularTemplate.html`
+    };
+}).controller('SampleDemoController', ['$scope', function ($scope) {
+    $scope.data = {
+        Title: 'Title',
+        Text: 'Text',
+        Image: '<img src="">'
+    };
+}]).filter('asTrusted', ['$sce', function ($sce) {
+    return function (text) {
+        return $sce.trustAsHtml(text);
+    };
+}])
